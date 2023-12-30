39d1ddd99af8ac1af9f45b3fd9f1a3c280039046
Roodly Stevenson Cherelus <tiroodly@gmail.com>
2023-12-30 13:27:47 -0500

    drawer done at 80%

--- lib/app/constants/app_theme.dart
+++ lib/app/constants/app_theme.dart
@@ -5,6 +5,7 @@ class AppTheme {
   static const Color appbarColor = Color(0xff26384f);
   static const Color shadow = Color.fromARGB(65, 0, 0, 0);
   static const Color darkGray = Color(0xff324149);
+  static Color darkGrayMenu = Colors.grey.shade800; //Color(0xff324149);
   static const Color lihtGray = Color(0xFFD3D3D3);
 
   static const Color hintLoginColor = Colors.white;
@@ -17,6 +18,7 @@ class AppTheme {
   static const Color textFieldStyleColor = Colors.white;
 
   static const Color richTextStyleColor = Colors.black;
+  static const Color blackTextStyleColor = Colors.black;
   static const Color textRichSpanStyleColor = Color(0XFF4321F5);
   static const Color iconButtonColor = Colors.white;
   static const Color buttonLoginBackgroundColor = Colors.white;
--- lib/app/modules/common/navigation_drawer.dart
+++ lib/app/modules/common/navigation_drawer.dart
@@ -1,13 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+import '../../../util.dart';
+import '../../constants/app_theme.dart';
+import '../../databases/boxes.dart';
 import '../../routes/app_pages.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+
+import '../authentication/controllers/authentication_controller.dart';
 
 class NavigationDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
       child: Container(
+        decoration: BoxDecoration(
+          //color: Colors.white,
+          borderRadius: const BorderRadius.only(
+              topLeft: Radius.circular(10),
+              topRight: Radius.circular(10),
+              bottomLeft: Radius.circular(10),
+              bottomRight: Radius.circular(10)),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.5),
+              spreadRadius: 5,
+              blurRadius: 7,
+              offset: const Offset(0, 3), // changes position of shadow
+            ),
+          ],
+        ),
         child: ListView(
           children: [
             buildDrawerHeader(),
@@ -18,7 +41,9 @@ class NavigationDrawer extends StatelessWidget {
               icon: Icons.home,
               text: "Home",
               onTap: () => navigate(0),
-              tileColor: Get.currentRoute == Routes.HOME ? Colors.blue : null,
+              tileColor: Get.currentRoute == Routes.HOME
+                  ? AppTheme.darkGrayMenu
+                  : null,
               textIconColor:
                   Get.currentRoute == Routes.HOME ? Colors.white : Colors.black,
             ),
@@ -26,7 +51,9 @@ class NavigationDrawer extends StatelessWidget {
               icon: Icons.person,
               text: "User",
               onTap: () => navigate(1),
-              tileColor: Get.currentRoute == Routes.USER ? Colors.blue : null,
+              tileColor: Get.currentRoute == Routes.USER
+                  ? AppTheme.darkGrayMenu
+                  : null,
               textIconColor:
                   Get.currentRoute == Routes.USER ? Colors.white : Colors.black,
             ),
@@ -34,8 +61,9 @@ class NavigationDrawer extends StatelessWidget {
                 icon: Icons.production_quantity_limits,
                 text: "Products",
                 onTap: () => navigate(2),
-                tileColor:
-                    Get.currentRoute == Routes.PRODUCT ? Colors.blue : null,
+                tileColor: Get.currentRoute == Routes.PRODUCT
+                    ? AppTheme.darkGrayMenu
+                    : null,
                 textIconColor: Get.currentRoute == Routes.PRODUCT
                     ? Colors.white
                     : Colors.black),
@@ -43,8 +71,9 @@ class NavigationDrawer extends StatelessWidget {
                 icon: Icons.category,
                 text: "Categories",
                 onTap: () => navigate(3),
-                tileColor:
-                    Get.currentRoute == Routes.CATEGORY ? Colors.blue : null,
+                tileColor: Get.currentRoute == Routes.CATEGORY
+                    ? AppTheme.darkGrayMenu
+                    : null,
                 textIconColor: Get.currentRoute == Routes.CATEGORY
                     ? Colors.white
                     : Colors.black),
@@ -52,8 +81,9 @@ class NavigationDrawer extends StatelessWidget {
                 icon: Icons.bar_chart,
                 text: "Orders",
                 onTap: () => navigate(4),
-                tileColor:
-                    Get.currentRoute == Routes.ORDER ? Colors.blue : null,
+                tileColor: Get.currentRoute == Routes.ORDER
+                    ? AppTheme.darkGrayMenu
+                    : null,
                 textIconColor: Get.currentRoute == Routes.ORDER
                     ? Colors.white
                     : Colors.black),
@@ -62,7 +92,7 @@ class NavigationDrawer extends StatelessWidget {
                 text: "Pending Orders",
                 onTap: () => navigate(5),
                 tileColor: Get.currentRoute == Routes.PENDING_ORDER
-                    ? Colors.blue
+                    ? AppTheme.darkGrayMenu
                     : null,
                 textIconColor: Get.currentRoute == Routes.PENDING_ORDER
                     ? Colors.white
@@ -72,7 +102,7 @@ class NavigationDrawer extends StatelessWidget {
                 text: "Cancelled Orders",
                 onTap: () => navigate(6),
                 tileColor: Get.currentRoute == Routes.CANCELLED_ORDER
-                    ? Colors.blue
+                    ? AppTheme.darkGrayMenu
                     : null,
                 textIconColor: Get.currentRoute == Routes.CANCELLED_ORDER
                     ? Colors.white
@@ -81,8 +111,9 @@ class NavigationDrawer extends StatelessWidget {
                 icon: Icons.settings,
                 text: "Settings",
                 onTap: () => navigate(7),
-                tileColor:
-                    Get.currentRoute == Routes.SETTINGS ? Colors.blue : null,
+                tileColor: Get.currentRoute == Routes.SETTINGS
+                    ? AppTheme.darkGrayMenu
+                    : null,
                 textIconColor: Get.currentRoute == Routes.SETTINGS
                     ? Colors.white
                     : Colors.black),
@@ -93,21 +124,74 @@ class NavigationDrawer extends StatelessWidget {
   }
 
   Widget buildDrawerHeader() {
-    return const UserAccountsDrawerHeader(
-      accountName: Text("Ripples Code"),
-      accountEmail: Text("ripplescode@gmail.com"),
-      currentAccountPicture: CircleAvatar(
-        backgroundImage: AssetImage('assets/images/appicon.png'),
-      ),
-      currentAccountPictureSize: Size.square(72),
-      otherAccountsPictures: [
-        CircleAvatar(
-          backgroundColor: Colors.white,
-          child: Text("RC"),
-        )
-      ],
-      otherAccountsPicturesSize: Size.square(50),
-    );
+    return ValueListenableBuilder(
+        valueListenable: Boxes.getStorageBox().listenable(),
+        builder: (context, Box box, _) {
+          final String imageString = box.get('profilePhoto', defaultValue: '');
+          final String userName = box.get('userName', defaultValue: '');
+          return UserAccountsDrawerHeader(
+            decoration: BoxDecoration(
+              color: Colors.grey.shade800,
+              borderRadius: const BorderRadius.only(
+                  // topLeft: Radius.circular(10),
+                  //topRight: Radius.circular(10),
+                  //  bottomLeft: Radius.circular(10),
+                  /*  bottomRight: Radius.circular(10) */),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.5),
+                  spreadRadius: 5,
+                  blurRadius: 7,
+                  offset: const Offset(0, 3), // changes position of shadow
+                ),
+              ],
+            ),
+            accountName: Text(
+              userName,
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+              style: const TextStyle(
+                  color: AppTheme.hintLoginColor,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 19),
+            ),
+            accountEmail: Get.find<AuthenticationController>().email != null
+                ? Text(Get.find<AuthenticationController>().email!)
+                : const Text('Guest'),
+            currentAccountPicture:
+                Get.find<AuthenticationController>().imageurl != null
+                    ? Image.network(
+                        Get.find<AuthenticationController>().imageurl!,
+                        width: 30,
+                        height: 30,
+                      )
+                    : CircleAvatar(
+                        backgroundImage: AssetImage(
+                            Get.find<AuthenticationController>().user != null
+                                ? 'assets/images/person_connected.png'
+                                : 'assets/images/person_disconnected.png'),
+                      ),
+            currentAccountPictureSize: const Size.square(72),
+            otherAccountsPictures: [
+              CircleAvatar(
+                backgroundColor: Colors.white,
+                child: Text(
+                  Get.find<AuthenticationController>().user != null
+                      ? Get.find<AuthenticationController>()
+                          .user!
+                          .email!
+                          .substring(0, 2)
+                          .toUpperCase()
+                      : 'GS',
+                  style: const TextStyle(
+                      color: AppTheme.blackTextStyleColor,
+                      fontWeight: FontWeight.bold),
+                ),
+              )
+            ],
+            otherAccountsPicturesSize: const Size.square(50),
+          );
+        });
   }
 
   Widget buildDrawerItem({
@@ -129,15 +213,6 @@ class NavigationDrawer extends StatelessWidget {
   }
 
   navigate(int index) {
-    /* if (index == 0) {
-      Get.toNamed(Routes.HOME);
-    } else if (index == 1) {
-      Get.toNamed(Routes.USER);
-    }
-    if (index == 2) {
-      Get.toNamed(Routes.PRODUCT);
-    } */
-
     switch (index) {
       case 0:
         return Get.toNamed(AppPages.HOME);
